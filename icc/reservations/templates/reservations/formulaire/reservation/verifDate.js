44e39eb87b66f6fb40c98a5f2d2ad2de46a0603e
Faton-edu <elv-faton.zmr@eduge.ch>
2021-12-19 14:53:20 +0100

    création script popup

--- icc/reservations/templates/reservations/formulaire/reservation/verifDate.js
+++ icc/reservations/templates/reservations/formulaire/reservation/verifDate.js
@@ -1,2 +1,13 @@
 var date = document.getElementById(id_date);
 
+date.value
+
+function errorIfDateIsPast(a){
+    if (a.value < Date() ){
+        document.body.style.backgroundColor = 'red'
+    }
+}
+
+function getDateSelected(id_date){
+    return document.getElementById(id_date)
+}
